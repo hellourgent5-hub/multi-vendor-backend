0e93d7894e01ad4c266b0c4edde0a827e4540d0d
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-17 17:54:22 +0530

    Delete backend directory

--- backend/routes/vendorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerVendor, loginVendor } = require('../controllers/vendorController');
-
-router.post('/register', registerVendor);
-router.post('/login', loginVendor);
-
-module.exports = router;
-
