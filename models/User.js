2d4bce74d88c66f1f7ff26eb8b3649b7be474b8a
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-18 17:28:05 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,27 +1,23 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['customer','admin'], default: 'customer' },
-  // simple geo (optional)
-  location: {
-    type: { type: String, default: 'Point' },
-    coordinates: { type: [Number], default: [0,0] } // [lng, lat]
-  }
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['customer','vendor','admin'], default: 'customer' }
 }, { timestamps: true });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+// Hash password before saving
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
-userSchema.methods.matchPassword = async function(entered) {
-  return await bcrypt.compare(entered, this.password);
-};
+// Method to compare password
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
-module.exports = mongoose.model('User', userSchema);
+export default mongoose.model('User', userSchema);
