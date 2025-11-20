bd9ccc9b77c35222e24dc61519e86192356cf710
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-20 10:45:21 +0530

    Update moduleRoutes.js

--- routes/moduleRoutes.js
+++ routes/moduleRoutes.js
@@ -3,23 +3,10 @@ import Module from "../models/Module.js";
 
 const router = express.Router();
 
-// GET all modules
 router.get("/", async (req, res) => {
   try {
     const modules = await Module.find();
     res.json(modules);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// POST create a module
-router.post("/", async (req, res) => {
-  const { name } = req.body;
-  try {
-    const newModule = new Module({ name });
-    const saved = await newModule.save();
-    res.status(201).json(saved);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
