3acb9723038f318d2c742cd7fc3404efaa8a5f3d
hellourgent5-hub <hellourgent5@gmail.com>
2025-11-20 11:27:23 +0530

    Update Module.js

--- models/Module.js
+++ models/Module.js
@@ -1,11 +1,7 @@
-// models/Module.js
 import mongoose from "mongoose";
 
 const moduleSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  }
-}, { timestamps: true });
+  name: { type: String, required: true, unique: true },
+});
 
 export default mongoose.model("Module", moduleSchema);
